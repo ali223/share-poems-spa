b676a3c38e98eb5edee09af1ace52547264141b1
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-07-14 13:26:56 +0100

    Vuex store - add the state, mutation and action for the poems

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,36 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue';
+import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    poems: []
+  },
+
+  mutations: {
+    setPoems(state, poems) {
+      state.poems = poems;
+    }
+  },
+
+  actions: {
+    fetchPoems({ commit }) {
+      return new Promise((resolve, reject) => {
+        axios
+          .get('http://share-poems-api.test/poems')
+          .then(response => {
+            let poems = response.data.data;
+            commit('setPoems', poems);
+            resolve(poems);
+          })
+          .catch(error => {
+            reject(error);
+          });
+      });
+    }
+  },
+
   modules: {}
 });
