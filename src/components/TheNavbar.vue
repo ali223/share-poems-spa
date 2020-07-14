bd47ab8e7de541a52b3a488b6879ddd93cf79a7d
Mohammed Ali Essa <ma.essa.pk@gmail.com>
2020-07-14 16:06:58 +0100

    Link the toolbar title to home page

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -2,15 +2,21 @@
   <nav>
     <v-app-bar flat color="teal darken-4" dark app>
       <v-toolbar-title>
-        <span class="site-title">Share Poems</span>
+        <span class="site-title">
+          <router-link :to="{ name: 'Home' }">
+            Share Poems
+          </router-link>
+        </span>
       </v-toolbar-title>
     </v-app-bar>
   </nav>
 </template>
 
 <style scoped>
-span.site-title {
+span.site-title a {
   font-family: 'Courgette', cursive;
   font-size: 1.8em;
+  color: white;
+  text-decoration: none;
 }
 </style>
